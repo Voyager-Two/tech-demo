f6d8e07579c69897cc2e90106c783199cb04f730
Emre <work@emre.earth>
2022-04-12 19:29:39 -0400

    test

--- src/client/tailwind.config.js
+++ src/client/tailwind.config.js
@@ -1,7 +1,12 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { join } = require('path');
+
 module.exports = {
   content: [
     // Doesn't work for some reason
     // Works inside postcss.config.js
+    join(__dirname, 'pages/**/*.{js,ts,jsx,tsx}'),
+    join(__dirname, 'features/**/*.{js,ts,jsx,tsx}'),
   ],
   // https://tailwindcss.com/docs/theme#customizing-the-default-theme
   theme: {
