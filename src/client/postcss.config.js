a3346e26e021bd88ccf7699a25eed5227cfe6ab7
Emre <work@emre.earth>
2022-04-12 18:26:20 -0400

    try

--- src/client/postcss.config.js
+++ src/client/postcss.config.js
@@ -1,8 +1,12 @@
+import { join } from 'path';
+
 module.exports = {
   plugins: {
     'postcss-import': {},
     'tailwindcss': {
       content: [
+        join(__dirname, 'src/client/pages/**/*.tsx'),
+        join(__dirname, 'src/client/features/**/*.tsx'),
         // "./src/client/pages/**/*.{js,ts,jsx,tsx}",
         // "./src/client/features/**/*.{js,ts,jsx,tsx}",
       ],
