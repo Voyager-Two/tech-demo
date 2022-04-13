2efeff105633227e7a497378952564f33069a86c
Emre <work@emre.earth>
2022-04-12 23:47:08 -0400

    try tailwind 2.x

--- src/client/postcss.config.js
+++ src/client/postcss.config.js
@@ -2,12 +2,7 @@ console.warn('postcss', __dirname);
 
 module.exports = {
   plugins: {
-    // 'tailwindcss': {
-    //   content: [
-    //     './src/client/pages/**/*.tsx',
-    //     './src/client/features/**/*.tsx',
-    //   ],
-    // },
+    'tailwindcss': {},
     'autoprefixer': {},
  },
 };
--- src/client/tailwind.config.js
+++ src/client/tailwind.config.js
@@ -1,18 +1,17 @@
 console.warn('tailwind', __dirname);
 
 module.exports = {
-  content: [
-    // Doesn't work for some reason
-    // Works inside postcss.config.js
+  mode: 'jit',
+  purge: [
     './src/client/pages/**/*.tsx',
     './src/client/features/**/*.tsx',
   ],
-  // https://tailwindcss.com/docs/theme#customizing-the-default-theme
+  darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-      // Add configs here to extend default theme
-    },
-    // Add config here if you want to override default theme
+    extend: {},
+  },
+  variants: {
+    extend: {},
   },
   plugins: [],
 }
